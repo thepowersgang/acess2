64c188e3116d5fc5d0d487791552414d472c0eea
John Hodge <tpg@portege.thepowersgang.com>
2009-09-22 11:27:38 +0800

    Fixed number of arguments to SYS_READ (should be 4 due to 64-bit argument)

--- Usermode/Libraries/libacess.so_src/vfs.asm
+++ Usermode/Libraries/libacess.so_src/vfs.asm
@@ -10,7 +10,7 @@
 SYSCALL2	open, SYS_OPEN
 SYSCALL3	reopen, SYS_REOPEN
 SYSCALL1	close, SYS_CLOSE
-SYSCALL3	read, SYS_READ
+SYSCALL4	read, SYS_READ	; int, int64_t, void*
 SYSCALL4	write, SYS_WRITE	; int, int64_t, void*
 SYSCALL4	seek, SYS_SEEK		; int, int64_t, int
 SYSCALL2	finfo, SYS_FINFO
