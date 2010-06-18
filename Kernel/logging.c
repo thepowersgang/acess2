32b42cd8c5cc03945f6e436259ae4c8252462d9b
John Hodge <tpg@mutabah.net>
2010-06-18 16:48:14 +0800

    Removed debug in module loader, shortened the length of the logging timestamps

--- Kernel/logging.c
+++ Kernel/logging.c
@@ -123,7 +123,7 @@ void Log_AddEvent(char *Ident, int Level, char *Format, va_list Args)
  */
 void Log_Int_PrintMessage(tLogEntry *Entry)
 {
-	LogF("%s%018lli%s [%+8s] %s\x1B[0m\n",
+	LogF("%s%014lli%s [%+8s] %s\x1B[0m\n",
 		csaLevelColours[Entry->Level],
 		Entry->Time,
 		csaLevelCodes[Entry->Level],
