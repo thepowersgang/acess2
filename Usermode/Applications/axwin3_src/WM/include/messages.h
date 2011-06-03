3613244ff0f7da156d25406bb5a0ad796237a7e4
John Hodge <tpg@mutabah.net>
2011-06-03 15:45:24 +0800

    Backup - Adding design for AxWin3 to git

---
+++ Usermode/Applications/axwin3_src/WM/include/messages.h
@@ -0,0 +1,13 @@
+/*
+ * Acess2 GUI (AxWin3)
+ */
+#ifndef _MESSAGES_H_
+#define _MESSAGES_H_
+
+enum eMessages
+{
+	MSG_NULL,
+};
+
+#endif
+
---
+++ Usermode/Applications/axwin3_src/WM/messageio.c
@@ -0,0 +1,22 @@
+/*
+ * Acess2 GUI (axwin3)
+ * By John Hodge (thePowersGang)
+ * 
+ * messageio.c
+ * - Common message handling code
+ */
+#include <common.h>
+#include <messages.h>
+
+// === PROTOTYPES ===
+
+// === GLOBALS ===
+
+// === CODE ===
+void DispachMessage(tAxWin_Message *Message)
+{
+	switch(Message->ID)
+	{
+	
+	}
+}
