2bc4a1c1fc602a92ace9fbeea5664c6977f8ae5b
John Hodge <tpg@mutabah.net>
2011-09-26 22:13:58 +0800

    Kernel/arm7 - Working on threading

--- Kernel/arch/arm7/proc.c
+++ Kernel/arch/arm7/proc.c
@@ -11,19 +11,33 @@
 
 // === IMPORTS ===
 extern tThread	gThreadZero;
+extern void	SwitchTask(Uint32 NewSP, Uint32 *OldSP, Uint32 NewIP, Uint32 *OldIP, Uint32 MemPtr);
 
 // === PROTOTYPES ===
+void	Proc_IdleThread(void *unused);
+tTID	Proc_NewKThread( void (*Fnc)(void*), void *Ptr );
 
 // === GLOBALS ===
 tThread	*gpCurrentThread = &gThreadZero;
+tThread *gpIdleThread = NULL;
 
 // === CODE ===
 void ArchThreads_Init(void)
 {
 }
 
+void Proc_IdleThread(void *unused)
+{
+	for(;;)
+		Proc_Reschedule();
+}
+
 void Proc_Start(void)
 {
+	tTID	tid;
+
+	tid = Proc_NewKThread( Proc_IdleThread, NULL );
+	gpIdleThread = Threads_GetThread(tid);
 }
 
 int GetCPUNum(void)
@@ -47,7 +61,13 @@ void Proc_StartUser(Uint Entrypoint, Uint *Bases, int ArgC, char **ArgV, char **
 
 tTID Proc_SpawnWorker( void (*Fnc)(void*), void *Ptr )
 {
-	return 0;
+	return -1;
+}
+
+tTID Proc_NewKThread( void (*Fnc)(void*), void *Ptr )
+{
+	// TODO: Implement
+	return -1;
 }
 
 void Proc_CallFaultHandler(tThread *Thread)
@@ -57,7 +77,20 @@ void Proc_CallFaultHandler(tThread *Thread)
 
 void Proc_Reschedule(void)
 {
-	// TODO: Task switching!
+	tThread	*cur, *next;
+
+	cur = gpCurrentThread;
+
+	next = Threads_GetNextToRun(0, cur);
+	if(!next)	next = gpIdleThread;
+	if(!next || next == cur)	return;
+	
+	SwitchTask(
+		next->SavedState.SP, &cur->SavedState.SP,
+		next->SavedState.IP, &cur->SavedState.IP,
+		next->MemState.Base
+		);
+	
 }
 
 void Proc_DumpThreadCPUState(tThread *Thread)
