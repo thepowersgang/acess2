9a70d1ed74070f56c34b856f88931b134d429c3a
John Hodge <tpg@mutabah.net>
2011-10-17 11:11:07 +0800

    Usermode/include - A pile of hack known as the Acess fcntl.h
    - Macros a few Acess syscalls into the posix equivalents

---
+++ Usermode/include/fcntl.h
@@ -0,0 +1,26 @@
+/*
+ * Acess2 C Library (UNIX Emulation)
+ * - By John Hodge (thePowersGang)
+ *
+ * fcntl.h
+ * - ??
+ */
+
+#ifndef _FCNTL_H_
+#define _FCNTL_H_
+
+#include <sys/sys.h>
+
+// Hacks to handle different behaviors in Acess
+
+// Open doesn't take a chmod
+#define open(_1,_2,...)	open(_1, _2)
+
+// Close returns void
+#define close(_1)	(close(_1),0)
+
+// Acess doesn't implement lseek
+#define lseek(_1,_2,_3)	(seek(_1,_2,_3),tell(_1))
+
+#endif
+
