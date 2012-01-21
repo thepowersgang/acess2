ceae45d80582dcb433cd26dcd6aad1c17229779d
John Hodge <tpg@mutabah.net>
2012-01-21 10:41:48 +0800

    Kernel - Split VTerm up into multiple files

---
+++ Kernel/drv/vterm_font.c
@@ -0,0 +1,250 @@
+/*
+ * Acess2 Kernel
+ * - By John Hodge (thePowersGang)
+ *
+ * drv/vterm_font.c
+ * - Virtual Terminal - Font rendering code
+ */
+#include "vterm.h"
+#include <api_drv_terminal.h>
+
+// ---
+// Font Render
+// ---
+#define MONOSPACE_FONT	10816
+
+#if MONOSPACE_FONT == 10808	// 8x8
+# include "vterm_font_8x8.h"
+#elif MONOSPACE_FONT == 10816	// 8x16
+# include "vterm_font_8x16.h"
+#endif
+
+// === PROTOTYPES ===
+Uint8	*VT_Font_GetChar(Uint32 Codepoint);
+
+// === GLOBALS ===
+int	giVT_CharWidth = FONT_WIDTH;
+int	giVT_CharHeight = FONT_HEIGHT;
+
+// === CODE ===
+/**
+ * \brief Render a font character
+ */
+void VT_Font_Render(Uint32 Codepoint, void *Buffer, int Depth, int Pitch, Uint32 BGC, Uint32 FGC)
+{
+	Uint8	*font;
+	 int	x, y;
+	
+	// 8-bpp and below
+	if( Depth <= 8 )
+	{
+		Uint8	*buf = Buffer;
+		
+		font = VT_Font_GetChar(Codepoint);
+		
+		for(y = 0; y < FONT_HEIGHT; y ++)
+		{
+			for(x = 0; x < FONT_WIDTH; x ++)
+			{
+				if(*font & (1 << (FONT_WIDTH-x-1)))
+					buf[x] = FGC;
+				else
+					buf[x] = BGC;
+			}
+			buf = (void*)( (tVAddr)buf + Pitch );
+			font ++;
+		}
+	}
+	// 16-bpp and below
+	else if( Depth <= 16 )
+	{
+		Uint16	*buf = Buffer;
+		
+		font = VT_Font_GetChar(Codepoint);
+		
+		for(y = 0; y < FONT_HEIGHT; y ++)
+		{
+			for(x = 0; x < FONT_WIDTH; x ++)
+			{
+				if(*font & (1 << (FONT_WIDTH-x-1)))
+					buf[x] = FGC;
+				else
+					buf[x] = BGC;
+			}
+			buf = (void*)( (tVAddr)buf + Pitch );
+			font ++;
+		}
+	}
+	// 24-bpp colour
+	// - Special handling to not overwrite the next pixel
+	//TODO: Endian issues here
+	else if( Depth == 24 )
+	{
+		Uint8	*buf = Buffer;
+		Uint8	bg_r = (BGC >> 16) & 0xFF;
+		Uint8	bg_g = (BGC >>  8) & 0xFF;
+		Uint8	bg_b = (BGC >>  0) & 0xFF;
+		Uint8	fg_r = (FGC >> 16) & 0xFF;
+		Uint8	fg_g = (FGC >>  8) & 0xFF;
+		Uint8	fg_b = (FGC >>  0) & 0xFF;
+		
+		font = VT_Font_GetChar(Codepoint);
+		
+		for(y = 0; y < FONT_HEIGHT; y ++)
+		{
+			for(x = 0; x < FONT_WIDTH; x ++)
+			{
+				Uint8	r, g, b;
+				
+				if(*font & (1 << (FONT_WIDTH-x-1))) {
+					r = fg_r;	g = fg_g;	b = fg_b;
+				}
+				else {
+					r = bg_r;	g = bg_g;	b = bg_b;
+				}
+				buf[x*3+0] = b;
+				buf[x*3+1] = g;
+				buf[x*3+2] = r;
+			}
+			buf = (void*)( (tVAddr)buf + Pitch );
+			font ++;
+		}
+	}
+	// 32-bpp colour (nice and easy)
+	else if( Depth == 32 )
+	{
+		Uint32	*buf = Buffer;
+		
+		font = VT_Font_GetChar(Codepoint);
+		
+		for(y = 0; y < FONT_HEIGHT; y ++)
+		{
+			for(x = 0; x < FONT_WIDTH; x ++)
+			{
+				if(*font & (1 << (FONT_WIDTH-x-1)))
+					buf[x] = FGC;
+				else
+					buf[x] = BGC;
+			}
+			buf = (Uint32*)( (tVAddr)buf + Pitch );
+			font ++;
+		}
+	}
+}
+
+/**
+ * \fn Uint32 VT_Colour12to24(Uint16 Col12)
+ * \brief Converts a 12-bit colour into 24 bits
+ */
+Uint32 VT_Colour12to24(Uint16 Col12)
+{
+	Uint32	ret;
+	 int	tmp;
+	tmp = Col12 & 0xF;
+	ret  = (tmp << 0) | (tmp << 4);
+	tmp = (Col12 & 0xF0) >> 4;
+	ret |= (tmp << 8) | (tmp << 12);
+	tmp = (Col12 & 0xF00) >> 8;
+	ret |= (tmp << 16) | (tmp << 20);
+	return ret;
+}
+/**
+ * \brief Converts a 12-bit colour into 15 bits
+ */
+Uint16 VT_Colour12to15(Uint16 Col12)
+{
+	Uint32	ret;
+	 int	tmp;
+	tmp = Col12 & 0xF;
+	ret  = (tmp << 1) | (tmp & 1);
+	tmp = (Col12 & 0xF0) >> 4;
+	ret |= ( (tmp << 1) | (tmp & 1) ) << 5;
+	tmp = (Col12 & 0xF00) >> 8;
+	ret |= ( (tmp << 1) | (tmp & 1) ) << 10;
+	return ret;
+}
+
+/**
+ * \brief Converts a 12-bit colour into any other depth
+ * \param Col12	12-bit source colour
+ * \param Depth	Desired bit deptj
+ * \note Green then blue get the extra avaliable bits (16:5-6-5, 14:4-5-5)
+ */
+Uint32 VT_Colour12toN(Uint16 Col12, int Depth)
+{
+	Uint32	ret;
+	Uint32	r, g, b;
+	 int	rSize, gSize, bSize;
+	
+	// Fast returns
+	if( Depth == 24 )	return VT_Colour12to24(Col12);
+	if( Depth == 15 )	return VT_Colour12to15(Col12);
+	// - 32 is a special case, it's usually 24-bit colour with an unused byte
+	if( Depth == 32 )	return VT_Colour12to24(Col12);
+	
+	// Bounds checks
+	if( Depth < 8 )	return 0;
+	if( Depth > 32 )	return 0;
+	
+	r = Col12 & 0xF;
+	g = (Col12 & 0xF0) >> 4;
+	b = (Col12 & 0xF00) >> 8;
+	
+	rSize = gSize = bSize = Depth / 3;
+	if( rSize + gSize + bSize < Depth )	// Depth % 3 == 1
+		gSize ++;
+	if( rSize + gSize + bSize < Depth )	// Depth % 3 == 2
+		bSize ++;
+	
+	// Expand
+	r <<= rSize - 4;	g <<= gSize - 4;	b <<= bSize - 4;
+	// Fill with the lowest bit
+	if( Col12 & 0x001 )	r |= (1 << (rSize - 4)) - 1;
+	if( Col12 & 0x010 )	r |= (1 << (gSize - 4)) - 1;
+	if( Col12 & 0x100 )	r |= (1 << (bSize - 4)) - 1;
+	
+	// Create output
+	ret  = r;
+	ret |= g << rSize;
+	ret |= b << (rSize + gSize);
+	
+	return ret;
+}
+
+/**
+ * \fn Uint8 *VT_Font_GetChar(Uint32 Codepoint)
+ * \brief Gets an index into the font array given a Unicode Codepoint
+ * \note See http://en.wikipedia.org/wiki/CP437
+ */
+Uint8 *VT_Font_GetChar(Uint32 Codepoint)
+{
+	 int	index = 0;
+	if(Codepoint < 128)
+		return &VTermFont[Codepoint*FONT_HEIGHT];
+	switch(Codepoint)
+	{
+	case 0xC7:	index = 128;	break;	// Ç
+	case 0xFC:	index = 129;	break;	// ü
+	case 0xE9:	index = 130;	break;	// é
+	case 0xE2:	index = 131;	break;	// â
+	case 0xE4:	index = 132;	break;	// ä
+	case 0xE0:	index = 133;	break;	// à
+	case 0xE5:	index = 134;	break;	// å
+	case 0xE7:	index = 135;	break;	// ç
+	case 0xEA:	index = 136;	break;	// ê
+	case 0xEB:	index = 137;	break;	// ë
+	case 0xE8:	index = 138;	break;	// è
+	case 0xEF:	index = 139;	break;	// ï
+	case 0xEE:	index = 140;	break;	// î
+	case 0xEC:	index = 141;	break;	// ì
+	case 0xC4:	index = 142;	break;	// Ä
+	case 0xC5:	index = 143;	break;	// Å
+	}
+	
+	return &VTermFont[index*FONT_HEIGHT];
+}
+
+EXPORTAS(&giVT_CharWidth, giVT_CharWidth);
+EXPORTAS(&giVT_CharHeight, giVT_CharHeight);
+EXPORT(VT_Font_Render);
+EXPORT(VT_Colour12to24);
---
+++ Kernel/drv/vterm_vt100.c
@@ -0,0 +1,295 @@
+/*
+ * Acess2 Kernel
+ * - By John Hodge (thePowersGang)
+ *
+ * drv/vterm_vt100.c
+ * - Virtual Terminal - VT100 (Kinda) Emulation
+ */
+#include "vterm.h"
+
+// === CONSTANTS ===
+const Uint16	caVT100Colours[] = {
+		// Black, Red, Green, Yellow, Blue, Purple, Cyan, Gray
+		// Same again, but bright
+		VT_COL_BLACK, 0x700, 0x070, 0x770, 0x007, 0x707, 0x077, 0xAAA,
+		VT_COL_GREY, 0xF00, 0x0F0, 0xFF0, 0x00F, 0xF0F, 0x0FF, VT_COL_WHITE
+	};
+
+// === CODE ===
+/**
+ * \brief Handle a standard large escape code
+ * 
+ * Handles any escape code of the form \x1B[n,...A where n is an integer
+ * and A is any letter.
+ */
+void VT_int_ParseEscape_StandardLarge(tVTerm *Term, char CmdChar, int argc, int *args)
+{
+	 int	tmp = 1;
+	switch(CmdChar)
+	{
+	// Left
+	case 'D':
+		tmp = -1;
+	// Right
+	case 'C':
+		if(argc == 1)	tmp *= args[0];
+		if( Term->Flags & VT_FLAG_ALTBUF )
+		{
+			if( (Term->AltWritePos + tmp) % Term->TextWidth == 0 ) {
+				Term->AltWritePos -= Term->AltWritePos % Term->TextWidth;
+				Term->AltWritePos += Term->TextWidth - 1;
+			}
+			else
+				Term->AltWritePos += tmp;
+		}
+		else
+		{
+			if( (Term->WritePos + tmp) % Term->TextWidth == 0 ) {
+				Term->WritePos -= Term->WritePos % Term->TextWidth;
+				Term->WritePos += Term->TextWidth - 1;
+			}
+			else
+				Term->WritePos += tmp;
+		}
+		break;
+	
+	// Erase
+	case 'J':
+		switch(args[0])
+		{
+		case 0:	// Erase below
+			break;
+		case 1:	// Erase above
+			break;
+		case 2:	// Erase all
+			if( Term->Flags & VT_FLAG_ALTBUF )
+			{
+				 int	i = Term->TextHeight;
+				while( i-- )	VT_int_ClearLine(Term, i);
+				Term->AltWritePos = 0;
+				VT_int_UpdateScreen(Term, 1);
+			}
+			else
+			{
+				 int	i = Term->TextHeight * (giVT_Scrollback + 1);
+				while( i-- )	VT_int_ClearLine(Term, i);
+				Term->WritePos = 0;
+				Term->ViewPos = 0;
+				VT_int_UpdateScreen(Term, 1);
+			}
+			break;
+		}
+		break;
+	
+	// Erase in line
+	case 'K':
+		switch(args[0])
+		{
+		case 0:	// Erase to right
+			if( Term->Flags & VT_FLAG_ALTBUF )
+			{
+				 int	i, max;
+				max = Term->Width - Term->AltWritePos % Term->Width;
+				for( i = 0; i < max; i ++ )
+					Term->AltBuf[Term->AltWritePos+i].Ch = 0;
+			}
+			else
+			{
+				 int	i, max;
+				max = Term->Width - Term->WritePos % Term->Width;
+				for( i = 0; i < max; i ++ )
+					Term->Text[Term->WritePos+i].Ch = 0;
+			}
+			VT_int_UpdateScreen(Term, 0);
+			break;
+		case 1:	// Erase to left
+			if( Term->Flags & VT_FLAG_ALTBUF )
+			{
+				 int	i = Term->AltWritePos % Term->Width;
+				while( i -- )
+					Term->AltBuf[Term->AltWritePos++].Ch = 0;
+			}
+			else
+			{
+				 int	i = Term->WritePos % Term->Width;
+				while( i -- )
+					Term->Text[Term->WritePos++].Ch = 0;
+			}
+			VT_int_UpdateScreen(Term, 0);
+			break;
+		case 2:	// Erase all
+			if( Term->Flags & VT_FLAG_ALTBUF )
+			{
+				VT_int_ClearLine(Term, Term->AltWritePos / Term->Width);
+			}
+			else
+			{
+				VT_int_ClearLine(Term, Term->WritePos / Term->Width);
+			}
+			VT_int_UpdateScreen(Term, 0);
+			break;
+		}
+		break;
+	
+	// Set cursor position
+	case 'H':
+		if( Term->Flags & VT_FLAG_ALTBUF )
+			Term->AltWritePos = args[0] + args[1]*Term->TextWidth;
+		else
+			Term->WritePos = args[0] + args[1]*Term->TextWidth;
+		//Log_Debug("VTerm", "args = {%i, %i}", args[0], args[1]);
+		break;
+	
+	// Scroll up `n` lines
+	case 'S':
+		tmp = -1;
+	// Scroll down `n` lines
+	case 'T':
+		if(argc == 1)	tmp *= args[0];
+		if( Term->Flags & VT_FLAG_ALTBUF )
+			VT_int_ScrollText(Term, tmp);
+		else
+		{
+			if(Term->ViewPos/Term->TextWidth + tmp < 0)
+				break;
+			if(Term->ViewPos/Term->TextWidth + tmp  > Term->TextHeight * (giVT_Scrollback + 1))
+				break;
+			
+			Term->ViewPos += Term->TextWidth*tmp;
+		}
+		break;
+	
+	// Set Font flags
+	case 'm':
+		for( ; argc--; )
+		{
+			 int	colour_idx;
+			// Flags
+			if( 0 <= args[argc] && args[argc] <= 8)
+			{
+				switch(args[argc])
+				{
+				case 0:	Term->CurColour = DEFAULT_COLOUR;	break;	// Reset
+				case 1:	Term->CurColour |= 0x80000000;	break;	// Bright
+				case 2:	Term->CurColour &= ~0x80000000;	break;	// Dim
+				}
+			}
+			// Foreground Colour
+			else if(30 <= args[argc] && args[argc] <= 37) {
+				// Get colour index, accounting for bright bit
+				colour_idx = args[argc]-30 + ((Term->CurColour>>28) & 8);
+				Term->CurColour &= 0x8000FFFF;
+				Term->CurColour |= (Uint32)caVT100Colours[ colour_idx ] << 16;
+			}
+			// Background Colour
+			else if(40 <= args[argc] && args[argc] <= 47) {
+				// Get colour index, accounting for bright bit
+				colour_idx = args[argc]-40 + ((Term->CurColour>>12) & 8);
+				Term->CurColour &= 0xFFFF8000;
+				Term->CurColour |= caVT100Colours[ colour_idx ];
+			}
+			else {
+				Log_Warning("VTerm", "Unknown font flag %i", args[argc]);
+			}
+		}
+		break;
+	
+	// Set scrolling region
+	case 'r':
+		if( argc != 2 )	break;
+		Term->ScrollTop = args[0];
+		Term->ScrollHeight = args[1] - args[0];
+		break;
+	
+	default:
+		Log_Warning("VTerm", "Unknown control sequence '\\x1B[%c'", CmdChar);
+		break;
+	}
+}
+
+/**
+ * \fn int VT_int_ParseEscape(tVTerm *Term, const char *Buffer)
+ * \brief Parses a VT100 Escape code
+ */
+int VT_int_ParseEscape(tVTerm *Term, const char *Buffer)
+{
+	char	c;
+	 int	argc = 0, j = 1;
+	 int	args[6] = {0,0,0,0};
+	 int	bQuestionMark = 0;
+	
+	switch(Buffer[0])
+	{
+	//Large Code
+	case '[':
+		// Get Arguments
+		c = Buffer[j++];
+		if(c == '?') {
+			bQuestionMark = 1;
+			c = Buffer[j++];
+		}
+		if( '0' <= c && c <= '9' )
+		{
+			do {
+				if(c == ';')	c = Buffer[j++];
+				while('0' <= c && c <= '9') {
+					args[argc] *= 10;
+					args[argc] += c-'0';
+					c = Buffer[j++];
+				}
+				argc ++;
+			} while(c == ';');
+		}
+		
+		// Get Command
+		if( ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+		{
+			if( bQuestionMark )
+			{
+				switch(c)
+				{
+				// DEC Private Mode Set
+				case 'h':
+					if(argc != 1)	break;
+					switch(args[0])
+					{
+					case 25:
+						Term->Flags &= ~VT_FLAG_HIDECSR;
+						break;
+					case 1047:
+						VT_int_ToggleAltBuffer(Term, 1);
+						break;
+					}
+					break;
+				case 'l':
+					if(argc != 1)	break;
+					switch(args[0])
+					{
+					case 25:
+						Term->Flags |= VT_FLAG_HIDECSR;
+						break;
+					case 1047:
+						VT_int_ToggleAltBuffer(Term, 0);
+						break;
+					}
+					break;
+				default:
+					Log_Warning("VTerm", "Unknown control sequence '\\x1B[?%c'", c);
+					break;
+				}
+			}
+			else
+			{
+				VT_int_ParseEscape_StandardLarge(Term, c, argc, args);
+			}
+		}
+		break;
+		
+	default:
+		Log_Notice("VTerm", "TODO: Handle short escape codes");
+		break;
+	}
+	
+	//Log_Debug("VTerm", "j = %i, Buffer = '%s'", j, Buffer);
+	return j;
+}
