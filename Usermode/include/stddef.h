19435a203712447b8a1e17bbe7141b4b1741d917
John Hodge <tpg@mutabah.net>
2012-01-26 14:27:50 +0800

    Missing files

---
+++ Usermode/include/stddef.h
@@ -0,0 +1,10 @@
+#ifndef _STDDEF_H_
+#define _STDDEF_H_
+
+#ifndef NULL
+# define NULL	((void*)0)
+#endif
+
+typedef unsigned int	size_t;
+
+#endif
---
+++ Usermode/include/unistd.h
@@ -0,0 +1,10 @@
+#ifndef _UNISTD_H_
+#define _UNISTD_H_
+
+#define	O_RDWR	(OPENFLAG_READ|OPENFLAG_WRITE)
+
+#include "acess/sys.h"
+
+
+#endif
+
