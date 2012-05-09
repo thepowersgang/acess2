5e38128725e578f32103c28bb0d3ff387a944770
John Hodge <tpg@mutabah.net>
2012-05-09 12:34:51 +0800

    Modules/VIARhineII - Working on a driver, doesn't do anything yet

--- BuildConf/x86/default.mk
+++ BuildConf/x86/default.mk
@@ -2,6 +2,7 @@
 MODULES += Storage/ATA
 MODULES += Storage/FDDv2
 MODULES += Network/NE2000 Network/RTL8139
+MODULES += Network/VIARhineII
 MODULES += Display/VESA
 MODULES += Display/BochsGA
 #MODULES += Display/VIAVideo
