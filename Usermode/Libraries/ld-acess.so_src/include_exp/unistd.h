be1f5320115622b322cb49b2780b6cc21b271eda
John Hodge <tpg@mutabah.net>
2012-05-27 13:08:18 +0800

    Usermode - Added O_RDWR and friends to unistd.h

--- Usermode/Libraries/ld-acess.so_src/include_exp/unistd.h
+++ Usermode/Libraries/ld-acess.so_src/include_exp/unistd.h
@@ -2,6 +2,9 @@
 #define _UNISTD_H_
 
 #define	O_RDWR	(OPENFLAG_READ|OPENFLAG_WRITE)
+#define O_WR	(OPENFLAG_WRITE)
+#define O_RD	(OPENFLAG_READ)
+#define O_CREAT	(OPENFLAG_CREATE)
 
 #include "acess/sys.h"
 
