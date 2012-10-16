0eb50bc9e604f654bdb0409bb66da03b733f906c
John Hodge (sonata) <tpg@mutabah.net>
2012-10-16 14:46:04 +0800

    Usermode/libc - scanf() and many other cleanups

--- Usermode/Libraries/libc.so_src/stdio_int.h
+++ Usermode/Libraries/libc.so_src/stdio_int.h
@@ -4,28 +4,35 @@
  * Configuration Options
  */
 #ifndef _STDIO_INT_H
-# define _STDIO_INT_H
+#define _STDIO_INT_H
+
+#include <sys/types.h>
+#include <stddef.h>
 
 // === CONSTANTS ===
-#define FILE_FLAG_MODE_MASK	0x07
-#define FILE_FLAG_MODE_READ		0x01
-#define FILE_FLAG_MODE_WRITE	0x02
-#define FILE_FLAG_MODE_EXEC		0x03
-#define FILE_FLAG_MODE_APPEND	0x04
-#define FILE_FLAG_M_EXT		0x10
+#define FILE_FLAG_MODE_MASK	0x0007
+#define FILE_FLAG_MODE_READ	0x0001
+#define FILE_FLAG_MODE_WRITE	0x0002
+#define FILE_FLAG_MODE_EXEC	0x0003
+#define FILE_FLAG_MODE_APPEND	0x0004
+#define FILE_FLAG_M_EXT 	0x0010
+#define FILE_FLAG_M_BINARY	0x0020
+#define FILE_FLAG_EOF   	0x0100
+#define FILE_FLAG_DIRTY 	0x0200
+#define FILE_FLAG_ALLOC 	0x1000
 
 // === TYPES ===
 struct sFILE {
-  	 int	FD;
   	 int	Flags;
+  	 int	FD;
+	off_t	Pos;	
+
 	#if DEBUG_BUILD
-	char	*FileName;
+	char	*FileName;	// heap
 	#endif
-	#if STDIO_LOCAL_BUFFER
-	char	*Buffer;
-	Uint64	BufferStart;
-	 int	BufferSize;
-	#endif
+	void	*Buffer;
+	off_t	BufferStart;
+	size_t	BufferSize;
 };
 
 #endif
