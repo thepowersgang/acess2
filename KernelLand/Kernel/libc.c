71bbd3999ce518ca1dcfd9a668ad104ae0cb213e
John Hodge <tpg@ucc.asn.au>
2012-12-20 10:00:31 +0800

    Kernel - Added strtoul and friends to acess.h

--- KernelLand/Kernel/libc.c
+++ KernelLand/Kernel/libc.c
@@ -15,11 +15,11 @@
 #define	RANDOM_SPRUCE	0xf12b039
 
 // === PROTOTYPES ===
+#if 0
 unsigned long long	strtoull(const char *str, char **end, int base);
 unsigned long	strtoul(const char *str, char **end, int base);
 signed long long	strtoll(const char *str, char **end, int base);
 signed long	strtol(const char *str, char **end, int base);
-#if 0
  int	atoi(const char *string);
  int	ParseInt(const char *string, int *Val);
 void	itoa(char *buf, Uint64 num, int base, int minLength, char pad);
