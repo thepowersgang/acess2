5fc2955d2f3e3ffcc10ce713317771b72842d910
John Hodge <tpg@mutabah.net>
2013-03-01 16:02:12 +0800

    Externals/cross-compiler - Magic patchfiles

---
+++ Externals/cross-compiler/patches/gcc/gcc/config/acess2.h
@@ -0,0 +1,14 @@
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()      \
+  do {                                \
+    builtin_define_std ("acess2");      \
+    builtin_define_std ("unix");      \
+    builtin_assert ("system=acess2");   \
+    builtin_assert ("system=unix");   \
+  } while(0);
+
+/*
+#undef TARGET_VERSION                                     // note that adding these two lines cause an error in gcc-4.7.0
+#define TARGET_VERSION fprintf(stderr, " (i386 acess2)"); // the build process works fine without them until someone can work out an alternative
+*/
+
