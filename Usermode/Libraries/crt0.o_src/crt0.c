e2eb3fb11c16b2a57f2cf97a74f796d2dceca1d2
John Hodge <tpg@mutabah.net>
2013-03-05 10:00:40 +0800

    Usermode/crt0 - Patching to support cross-compiler

--- Usermode/Libraries/crt0.o_src/crt0.c
+++ Usermode/Libraries/crt0.o_src/crt0.c
@@ -6,10 +6,14 @@
 typedef	void	(*exithandler_t)(void);
 typedef	void	(*constructor_t)(void);
 
+constructor_t	_crtbegin_ctors[0] __attribute__((section(".ctors")));
+
 exithandler_t	_crt0_exit_handler;
-extern constructor_t	_crtbegin_ctors[];
+//extern constructor_t	_crtbegin_ctors[];
 extern void	_exit(int status) __attribute__((noreturn));
 extern int	main(int argc, char *argv[], char **envp);
+
+void _start(int argc, char *argv[], char **envp) __attribute__ ((alias("start")));
 
 void start(int argc, char *argv[], char **envp)
 {
--- Usermode/Libraries/crt0.o_src/crtbegin.c
+++
@@ -1,2 +0,0 @@
-
-void	*_crtbegin_ctors[0] __attribute__((section(".ctors")));
