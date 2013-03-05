a6bca9642c0e2a321eaf34ffbd8b0cf0ce386545
John Hodge <tpg@mutabah.net>
2013-03-05 10:01:01 +0800

    Externals/cross-compiler - Compilation works (for some definitions)

--- Externals/cross-compiler/patches/gcc/gcc/config/acess2.h
+++ Externals/cross-compiler/patches/gcc/gcc/config/acess2.h
@@ -7,6 +7,8 @@
     builtin_assert ("system=unix");   \
   } while(0);
 
+#define LIB_SPEC	"-lc -lld-acess"
+
 /*
 #undef TARGET_VERSION                                     // note that adding these two lines cause an error in gcc-4.7.0
 #define TARGET_VERSION fprintf(stderr, " (i386 acess2)"); // the build process works fine without them until someone can work out an alternative
