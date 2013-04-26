c3914bc3bbe97ef474e044558674f57f6af85e0a
John Hodge <tpg@mutabah.net>
2013-04-26 13:23:29 +0800

    Usermode/libc - More errno values and signal symbolic names

--- Usermode/Libraries/libc.so_src/include_exp/signal.h
+++ Usermode/Libraries/libc.so_src/include_exp/signal.h
@@ -13,10 +13,24 @@ typedef void (*sighandler_t)(int);
 #define SIG_DFL	((void*)0)
 #define SIG_ERR	((void*)-1)
 
+#define SIGHUP	1
+#define SIGINT	2
+#define SIGQUIT	3
+#define SIGILL	4
 #define	SIGABRT	6
+#define SIGFPE	8
+#define SIGKILL	9
+#define SIGSEGV	11
+//#define SIGPIPE	13
+#define SIGALRM	14
+#define SIGTERM	15
+#define SIGUSR1	16
+#define SIGUSR2	17
 
 #define SIGPIPE	1001
 #define SIGCHLD	1002
+
+extern sighandler_t	signal(int signum, sighandler_t handler);
 
 extern int	raise(int sig);
 
