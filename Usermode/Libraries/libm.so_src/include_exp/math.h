e5624832d0c6535be32fb0452c4ce8e8637b0ad4
John Hodge <tpg@mutabah.net>
2013-06-08 17:37:33 +0800

    Usermode/libm - Stubbed up a quick libm (using gcc builtins)

---
+++ Usermode/Libraries/libm.so_src/include_exp/math.h
@@ -0,0 +1,13 @@
+/*
+ * Acess2 C Library (Maths)
+ * - By John Hodge (thePowersGang)
+ *
+ * math.h
+ * - C99 Header
+ */
+#ifndef _LIBM__MATH_H_
+#define _LIBM__MATH_H_
+
+extern double	pow(double x, double y);
+
+#endif
---
+++ Usermode/Libraries/libm.so_src/pow.c
@@ -0,0 +1,18 @@
+/*
+ * Acess2 C Library (Maths)
+ * - By John Hodge (thePowersGang)
+ *
+ * pow.c
+ * - pow(), powf() and powl()
+ */
+#include <math.h>
+
+// === CODE ===
+double pow(double x, double y)
+{
+	if( x == 1.0f )
+		return 1.0f;
+	if( y == 0.0f )
+		return 1.0f;
+	return __builtin_pow(x,y);
+}
