b85f2ca0e6967f8f490943145855bd027d310c9b
John Hodge <tpg@mutabah.net>
2013-06-17 12:04:47 +0800

    Usermode/libc - inttypes.h fixed to include stdint.h

--- Usermode/Libraries/libc.so_src/include_exp/inttypes.h
+++ Usermode/Libraries/libc.so_src/include_exp/inttypes.h
@@ -1,13 +1,28 @@
+/*
+ * Acess2 C Library
+ * - By John Hodge (thePowersGang)
+ *
+ * inttypes.h
+ * - Fixed-size integer sizes
+ *
+ * Defined in IEEE Std 1003.1
+ */
+#ifndef _INTTYPES_H_
+#define _INTTYPES_H_
+
+#include <stdint.h>
 
 #define PRId64	"lld"
 #define PRIdLEAST64	"lld"
 #define PRIdFAST64	"lld"
 #define PRIdMAX
 #define PRIdPTR
-#define PRIi64
+#define PRIi64	"lli"
 #define PRIiLEAST64
 #define PRIiFAST64
 #define PRIiMAX
 #define PRIiPTR
 
 #define PRIx64	"llx"
+
+#endif
