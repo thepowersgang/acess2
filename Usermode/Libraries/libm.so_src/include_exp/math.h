6afcf1f9b14d9ad0111daea549cb0db5bf7f9009
John Hodge <tpg@mutabah.net>
2014-05-10 17:18:31 +0800

    Usermode/libm - Add hacky INFINITY and NAN macros

--- Usermode/Libraries/libm.so_src/include_exp/math.h
+++ Usermode/Libraries/libm.so_src/include_exp/math.h
@@ -12,6 +12,13 @@
 extern "C" {
 #endif
 
+typedef float	float_t;
+typedef double	double_t;
+
+
+#define INFINITY	(*(float*)((uint32_t[]){0x78000000}))
+#define NAN	(*(float*)((uint32_t[]){0x78000001}))
+
 extern double	pow(double x, double y);
 extern double	exp(double x);
 extern double	log(double val);
