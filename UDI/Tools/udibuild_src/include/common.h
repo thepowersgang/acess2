955dea0bc767f12aa8d53cfdcae60240c05674ca
John Hodge <tpg@mutabah.net>
2014-05-24 23:05:55 +0800

    UDI/udibuild - Added command tracing flag, C99 pedantic in default config

--- UDI/Tools/udibuild_src/include/common.h
+++ UDI/Tools/udibuild_src/include/common.h
@@ -9,12 +9,15 @@
 #define _COMMON_H_
 
 #include <stdarg.h>
+#include <stdbool.h>
 
 #ifndef __GNUC__
 # define __attribute__(...)
 #endif
 
 extern char	*mkstr(const char *fmt, ...) __attribute__((format(printf,1,2)));
+
+extern bool	gbTraceEnabled;
 
 #endif
 
