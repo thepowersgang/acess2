c3407f035bb2c887fe35d403d52d3bda3f1d5295
John Hodge <tpg@mutabah.net>
2014-05-26 17:45:12 +0800

    Usermode/crt0 - Replaced crtbegin/crtend with crti/crtn

--- Usermode/Libraries/crt0.o_src/crt0.c
+++ Usermode/Libraries/crt0.o_src/crt0.c
@@ -10,6 +10,8 @@ constructor_t	_crtbegin_ctors[0] __attribute__((section(".ctors")));
 
 exithandler_t	_crt0_exit_handler;
 //extern constructor_t	_crtbegin_ctors[];
+extern void	_init(void);
+extern void	_fini(void);
 extern void	_exit(int status) __attribute__((noreturn));
 extern int	main(int argc, char *argv[], char **envp);
 
@@ -22,11 +24,13 @@ void start(int argc, char *argv[], char **envp)
 	
 	for( i = 0; _crtbegin_ctors[i]; i ++ )
 		_crtbegin_ctors[i]();
+	
+	_init();
 
 	rv = main(argc, argv, envp);
 	
 	if( _crt0_exit_handler )
 		_crt0_exit_handler();
-
+	_fini();
 	_exit(rv);
 }
