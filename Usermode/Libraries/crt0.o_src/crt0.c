ac1ebb0d8f390a13be96f4cb6186f50473817d34
John Hodge <tpg@mutabah.net>
2014-05-31 13:36:52 +0800

    Usermode/crt0 - Added shared version for libraries

--- Usermode/Libraries/crt0.o_src/crt0.c
+++ Usermode/Libraries/crt0.o_src/crt0.c
@@ -9,7 +9,6 @@ typedef	void	(*constructor_t)(void);
 constructor_t	_crtbegin_ctors[0] __attribute__((section(".ctors")));
 
 exithandler_t	_crt0_exit_handler;
-//extern constructor_t	_crtbegin_ctors[];
 extern void	_init(void);
 extern void	_fini(void);
 extern void	_exit(int status) __attribute__((noreturn));
@@ -19,15 +18,13 @@ void _start(int argc, char *argv[], char **envp) __attribute__ ((alias("start"))
 
 void start(int argc, char *argv[], char **envp)
 {
-	 int	i;
-	 int	rv;
-	
-	for( i = 0; _crtbegin_ctors[i]; i ++ )
+	// TODO: isn't this handled by _init?
+	for( int i = 0; _crtbegin_ctors[i]; i ++ )
 		_crtbegin_ctors[i]();
 	
 	_init();
 
-	rv = main(argc, argv, envp);
+	int rv = main(argc, argv, envp);
 	
 	if( _crt0_exit_handler )
 		_crt0_exit_handler();
